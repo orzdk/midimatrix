dc5c06dbc28da84506b9f68480d693426be8eb1e
Jesper Christensen <jeschrper@hotmail.com>
2019-09-08 21:35:03 +0200

    20190809-2

--- js/usbmidiklik.js
+++ js/usbmidiklik.js
@@ -1,61 +1,71 @@
 
 var ip = "192.168.0.23:7001";
+var routeTable = [];
 
 printer = (msg) => {
 
 	$("#midiklik").append(msg);
 }
 
-connectSerialMode = () => {
-	ajaxPost('api/bootconnectserialmode',{}, function(midiReply){
-		printer(JSON.stringify(midiReply));
-	});	
+getRoutingInfo = () => {
+
+	ajaxPost('api/getRoutingInfo',{},()=>{});	
 }
 
-bootSerialMode = () => {
-	ajaxPost('api/bootserialmode',{}, function(midiReply){
-		printer(JSON.stringify(midiReply));
-	});	
+resetMidiKlik = () => {
+
+	ajaxPost('api/resetMidiKlik',{},()=>{});
 }
 
-menuChoose = () => {
-	ajaxPost('api/menuchoose',{}, function(midiReply){
-		printer(JSON.stringify(midiReply));
-	});	
+sendSysEx = (sysex) => {
+
+	ajaxPost('api/sendsysex',{sysex:sysex},()=>{});
 }
 
-resetMidiklik = () => {
-	ajaxPost('api/resetmidiklik',{}, function(midiReply){
-		printer(JSON.stringify(midiReply));
-	});	
+filterMask = (row, offset) => {
+	var bin="";
+	for(var i=0;i<4;i++){ bin += String(routeTable[row][i+offset]) }
+	return "0" + ConvertBase.bin2hex(bin).toString().toUpperCase();
 }
 
-getConfig = callback => {
-	ajaxPost('api/getconfig',{}, function(midiReply){
-		callback(midiReply);
-	});	
+createSysExOnClick = (row) => {
+	console.log("createSysExOnClick",row);
+	sourceType = row < 4 ? "00" : "01";
+	id = row > 3 ? "0" + String(row - 4) : "0" + String(row);
+	filterFilterMask = filterMask(row,0);
+	cableFilterMask = filterMask(row,4);
+	jackFilterMask = filterMask(row,8);
+
+	sysEx = "F0 77 77 78 01 " + sourceType + " " + id + " " + filterFilterMask + " " + cableFilterMask + " " + jackFilterMask + " " + "F7";
+	console.log(sysEx);
+	return sysEx;
 }
 
-checkClick = (x,y, event) => {
+checkClick = (row,col,event) => {
+	routeTable[row][col] = routeTable[row][col] == 0 ? 1 : 0;
 
-	$("#d"+event.target.id).addClass("dirty");
+	sysex = createSysExOnClick(row)
+	sendSysEx(sysex);
+	$("#m").val(sysex);
 }
 
-rendercheckboxes = configData => {
+renderCheckboxes = () => {
 
-	var t = "<table>";
+    var t = "";
  
-	for (i=0;i<12;i++){
+	for (row=0;row<12;row++){
 		t += "<tr>";    
-		for (k=0;k<12;k++){
-			checked = configData[i][k].optionValue ? " checked " : ""; 
-			t += "<td><div id='db" + k + i + "'><input id='b" + k + i + "' onclick='checkClick(" + k + "," + i + ",event)' type='checkbox'" + checked + "></div></td>";    
+		for (col=0;col<12;col++){
+			checked = routeTable[row][col] == 1 ? " checked " : ""; 
+			disabled = routeTable[row][col] ==  null ? " disabled " : ""; 
+			divid = "dd" + row + col;
+			inputid = "d" + row + col;
+			t += "<td><div id='" + divid + "'><input id='" + inputid + "' onclick='checkClick(" + row + "," + col + ",event)' type='checkbox'" + checked + disabled + "></div></td>";    
 		}
 		t += "</tr>";
 	}
-	t += "</table>";
 
-	$("body").append(t);
+	$("#routeTableContainer").html("<table>" + t + "</table>");
 }
 
 $(document).ready(() => {
@@ -66,8 +76,16 @@ $(document).ready(() => {
 		printer(msg);
 	});
 
-	getConfig(data => {
-		rendercheckboxes(data.routes);
+	socket.on("midiklik_obj", (msg, d) => {
+		routeMsg = JSON.parse(msg);
+		if (routeMsg.routeTable.length > 0){
+			routeTable = routeMsg.routeTable
+			renderCheckboxes();
+		}
 	});
 
+
 });
+
+
+
