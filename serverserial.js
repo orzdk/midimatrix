50d4276e6e104e3bd9eff7060ef8444484ef4a20
Jesper Christensen <jeschrper@hotmail.com>
2019-09-08 21:34:19 +0200

    20190809

--- serverserial.js
+++ serverserial.js
@@ -18,28 +18,35 @@ var server = app.listen(port);
 var socket = require("socket.io").listen(server);
 var apiRoutes = express.Router(); 
 
-var currentFunction = "";
 var serialBuffer = [];
 var serialPort = {};
+var serialConnected = false;
+
 var midiklikname = 'USB MIDIKliK 4x4';
-var routeInfoObjGlob = {};
 
 var lineSwitchesTemplate = {
-	0:  { pos:10, controller: "FILTER_CH"  },
-	1:  { pos:13, controller: "FILTER_SC"  },
-	2:  { pos:16, controller: "FILTER_RT"  },
-	3:  { pos:19, controller: "FILTER_SX"  },
-	4:  { pos:24, controller: "CABLE_IN_1" },
-	5:  { pos:26, controller: "CABLE_IN_2" },
-	6:  { pos:28, controller: "CABLE_IN_3" },
-	7:  { pos:30, controller: "CABLE_IN_4" },
-	8:  { pos:35, controller: "JACK_OUT_1" },
-	9:  { pos:37, controller: "JACK_OUT_2" },
-	10: { pos:39, controller: "JACK_OUT_3" },
-	11: { pos:41, controller: "JACK_OUT_4" }
+	0: {position:10, controller: "FILTER_CH"},
+	1: {position:13, controller: "FILTER_SC"},
+	2: {position:16, controller: "FILTER_RT"},
+	3: {position:19, controller: "FILTER_SX"},
+	4: {position:24, controller: "CABLE_IN_1"},
+	5: {position:26, controller: "CABLE_IN_2"},
+	6: {position:28, controller: "CABLE_IN_3"},
+	7: {position:30, controller: "CABLE_IN_4"},
+	8: {position:35, controller: "JACK_OUT_1"},
+	9: {position:37, controller: "JACK_OUT_2"},
+	10: {position:39, controller: "JACK_OUT_3"},
+	11: {position:41, controller: "JACK_OUT_4"}
 }
 
-var switchValue = (s) => s == "X" ? true : s == "." ? false : null;
+String.prototype.replaceAll = function(search, replace) {
+
+    var that = this;
+    return that.replace(new RegExp(search, 'g'), replace);
+
+}
+
+var switchValue = (s) => s == "X" ? 1 : s == "." ? 0 : null;
 
 var executeShellCommand = (cmd, callback) => {
 	exec(cmd, function(err, stdout, stderr){
@@ -48,13 +55,16 @@ var executeShellCommand = (cmd, callback) => {
 }
 
 var midiKlikAlsaID = (callback) => {
+	var rv = false;
 	executeShellCommand('sudo aconnect -l', function(data){
 		var lines = data.stdout.split('\n');
 		for (var c=0; c<lines.length; c++){
 			if (lines[c].indexOf(midiklikname) > -1 && lines[c].indexOf("type=kernel") > -1){
-				callback(lines[c].split(":")[0].split(" ")[1]);
+				rv = lines[c].split(":")[0].split(" ")[1];
+				break;
 			}
 		}
+		callback(rv);
 	});
 }
 
@@ -64,147 +74,186 @@ var midiKlikDeviceID = (callback) => {
 		for (var c=0; c<lines.length; c++){
 			if (lines[c].indexOf("1eaf") > -1){
 				callback(lines[c].split(" ")[3].substring(0,3));
+				break;
 			}
 		}
 	});
 }
 
-var bootMidiKlikSerialMode = (callback) => {
-	midiKlikAlsaID((midiklikalsaid)=>{
-		var cmd = 'send_midi ' + midiklikalsaid + ':0 SYSEX,F0,77,77,78,08,F7';
-		executeShellCommand(cmd, function(){
-			callback();
-		});	
-	});
+var isSerialPortAvailable = (callback) => {
+	executeShellCommand('ls /dev/ttyACM0', function(reply){
+		callback(reply.stdout.indexOf('/dev/ttyACM0') > -1);
+	});	
 }
 
-var resetMidiKlik = (callback) => {
-	midiKlikDeviceID(id=>{
-		var cmd1 = 'sudo /home/pi/midimatrix/tools/usbreset /dev/bus/usb/001/' + id;
-		executeShellCommand(cmd1, function(shellReply){
-			callback();
-		});
-	});
-}
+var processRoutingTable = (configLineArray, callback) => {
 
-var processRoutingTable = function(configLineArray, callback){
+	var routingTable = [];
 
-	var optionLineSequence = 0;
-	var routeInfoObj = {}
+	for (var lineNum=0; lineNum<configLineArray.length; lineNum++){
 
-	var attachInfoTo = null;
+		textLine = configLineArray[lineNum];
 
-	for (var configLine=0; configLine<configLineArray.length; configLine++){
-
-		line = configLineArray[configLine];
-		cableIndex = Number(line.substring(3,4))-1;
-
-		if(line.indexOf("->") > -1){
-
+		if(textLine.indexOf("->") > -1){
 			var lineSwitches = JSON.parse(JSON.stringify(lineSwitchesTemplate));
 
+			var routingTableLine = [];
+
 			for (var i=0;i<12;i++){
-				lineSwitches[i].optionValue = switchValue(line.substring(lineSwitches[i].pos,lineSwitches[i].pos+1));
+				switchPos = lineSwitches[i].position;
+				routingTableLine.push(switchValue(textLine.substring(switchPos,switchPos+1)));
 			}
 
-			routeInfoObj[optionLineSequence] = lineSwitches;
-
-			if (optionLineSequence-1 < 4){
-				routeInfoObj[optionLineSequence]["optionInfo"] = { group: "CABLE_OUT", cableIndex }
-			} else if (optionLineSequence >= 4 && optionLineSequence < 8 ){
-				routeInfoObj[optionLineSequence]["optionInfo"]= { group: "JACK_IN", cableIndex }
-			} else {
-				routeInfoObj[optionLineSequence]["optionInfo"] = { group: "JACK_IN_THRU", cableIndex }
-			}
-
-			optionLineSequence++;
+			routingTable.push(routingTableLine);
 		}
-	
 	}
 
-	callback(routeInfoObj);
+	callback(routingTable);
 }
 
-var createSysExMessage = (action, sourcetype, id, filtermask, targets) =>{
+var sendSysEx = (midiklikalsaid,sysEx,callback) => {
 
-	var sysExStart = "F0 77 77 78 0F";
-	var sysExEnd = "F7";
-	var sysEx;
-	var sourceType;
+	var cmd = 'send_midi ' + midiklikalsaid + ':0 SYSEX,' + sysEx.toString().replaceAll(" ",",");
+	console.log(cmd);
+	executeShellCommand(cmd, function(rply){
+		setTimeout(() => {
+			callback();
+		},2000);
+	});	
+}
 
-	if (action="reset"){
-		sysex = sysExStart + "00" + sysExEnd;
-	} else {
-		sourceType = sourcetype == "cable" ? "00" : "01";
+var bootMidiKlikSerialMode = (midiklikalsaid, callback) => {
+
+	sendSysEx(midiklikalsaid, "F0 77 77 78 08 F7", () => {
+		setTimeout(() => {
+			callback();
+		},2000);
+
+	});
+}
+
+var onSerial = (serialData) => {
+
+	line = serialData.toString();
+	serialBuffer.push(line);		  
+
+	if (line.indexOf("6.USB product string") > -1){
+		processRoutingTable(serialBuffer, (routeTable) => {
+	  		socket.sockets.emit("midiklik_obj", JSON.stringify({routeTable}));		
+  			serialBuffer = [];
+			serialData = "";
+	  	});
 	}
 }
 
-socket.on("connection", (client) => {
+var connectSerial = (delay, callback) => {
+	serialBuffer = [];
+	serialData = "";
 
-	var connectSerial = () => {
+	serialPort = new SerialPort("/dev/ttyACM0", {
+	    baudRate : 115200,
+	    dataBits : 8,
+	    parity : 'none',
+	    stopBits: 1,
+	    flowControl : false
+	}); 
 
-		serialPort = new SerialPort("/dev/ttyACM0", {
-		    baudRate : 115200,
-		    dataBits : 8,
-		    parity : 'none',
-		    stopBits: 1,
-		    flowControl : false
-		}); 
-	
-		const lineStream = serialPort.pipe(new Readline("\r\n"));
-	
-		lineStream.on('data', function (j) {
+	const lineStream = serialPort.pipe(new Readline("\r\n"));
+	lineStream.on('data', onSerial)
+	serialConnected = true;
 
-		  line = j.toString();
-		  serialBuffer.push(line);		  
-	
-		  if (line.indexOf("6.USB product string") > -1){
-		  	if (currentFunction == "CURRENT_SETTINGS"){
-		  		processRoutingTable(serialBuffer, routeInfoObj => {
-		  			routeInfoObjGlob = routeInfoObj;
-		  			socket.sockets.emit("midiklik", JSON.stringify(routeInfoObj));
-		  		});
-		  		currentFunction = "";
-		  	} 
-		  }
-		  
-		})
-
+	if (callback){
+ 		setTimeout(()=>{
+			callback();
+		},delay);	
 	}
+}
 
-	apiRoutes.post('/getconfig', (req, res) => {	
-		console.log("hest",routeInfoObjGlob);
-		res.json({routes: routeInfoObjGlob});
+apiRoutes.post('/getRoutingInfo', (req, res) => {	
+
+	socket.sockets.emit("midiklik", "Please wait for isSerialPortAvailable()\n");
+	isSerialPortAvailable((serialAvailable) => {
+
+		if(serialAvailable == true){
+			if(serialConnected == true){
+				serialPort.write("0");
+			} else {
+				socket.sockets.emit("midiklik", "Please wait for ConnectSerial()\n");
+				connectSerial(2000,()=>{
+					serialPort.write("0");
+				});
+			}
+		} else {
+			midiKlikAlsaID((midiklikalsaid) => {
+				if (midiklikalsaid != false){
+					socket.sockets.emit("midiklik", "Please wait for bootMidiKlikSerialMode()\n");
+					bootMidiKlikSerialMode(midiklikalsaid, () => {
+						socket.sockets.emit("midiklik", "Please wait for ConnectSerial()\n");
+						connectSerial(2000,()=>{
+							serialPort.write("0");
+						});			
+					});
+				};
+			});
+		}
+
 	});
 
-	apiRoutes.post('/resetmidiklik', (req, res) => {	
-		resetMidiKlik(()=>{
+	res.sendStatus(200);
+	
+});
+
+apiRoutes.post('/resetMidiKlik', (req, res) => {	
+ 
+ 	socket.sockets.emit("midiklik", "Please wait for midiKlikAlsaID()\n");
+	midiKlikAlsaID(midiklikalsaid => {
+ 	
+ 		socket.sockets.emit("midiklik", "Please wait for isSerialPortAvailable()\n");
+		isSerialPortAvailable(serialAvailable => {
+
+			if (serialAvailable == true){
+
+				if (serialConnected == true){
+					serialPort.write("s");
+					socket.sockets.emit("midiklik", "Reset\n");	
+				} else {
+					connectSerial(()=>{
+						setTimeout(()=>{
+							serialPort.write("s");
+							socket.sockets.emit("midiklik", "Connected and reset\n");	
+						},1000);
+						
+					});
+				}
+
+			} else {
+				socket.sockets.emit("midiklik", "Midiklik is not in serial mode\n");
+			}
+
+		});
+
+	});
+	
+	res.sendStatus(200);
+
+});
+
+apiRoutes.post('/sendsysex', (req, res) => {	
+
+	/* Her skal først slås tilbage til MidiMode !!!! */
+
+ 	socket.sockets.emit("midiklik", "Please wait for midiKlikAlsaID()\n");
+	midiKlikAlsaID(midiklikalsaid => {
+
+	 	socket.sockets.emit("midiklik", "Please wait for sendSysEx()\n");
+		sendSysEx(midiklikalsaid, req.body.sysex, () => {
 			res.sendStatus(200);
 		});
 	});
-
-	apiRoutes.post('/menuchoose', (req, res) => {	
-		currentFunction = "CURRENT_SETTINGS";
-		serialPort.write("0");		
-		res.sendStatus(200);
-	});
-
-	apiRoutes.post('/bootserialmode', (req, res) =>{	
-		bootMidiKlikSerialMode(()=>{
-			res.sendStatus(200);		
-		});
-	});
-
-	apiRoutes.post('/bootconnectserialmode', (req, res) => {	
-		bootMidiKlikSerialMode(()=>{
-			setTimeout(()=>{
-				connectSerial();
-				res.sendStatus(200);		
-			},2000);	
-		});
-	});
 	
-	app.use('/api', apiRoutes);
-
 });
+
+app.use('/api', apiRoutes);
+
+
 
